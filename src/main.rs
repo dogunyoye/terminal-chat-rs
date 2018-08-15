6001a476a44f133f5812b43184db2856db672906
Dimeji Ogunyoye <dimejiogunyoye@googlemail.com>
2018-08-15 16:22:05 +0100

    Adding option selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::process::Command;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let output = Command::new("clear").output().unwrap_or_else(|e| {
+        panic!("failed to execute process: {}", e)
+    });
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+
+    let row = "*".repeat(38);
+    let welcome_line = "*    Welcome to terminal-chat-rs!    *";
+    let author_line = "*    @author Dimeji Ogunyoye         *";
+
+    let create_server_option = "[1] Create server";
+    let remove_server_option = "[2] Remove server";
+    let join_room_option = "[3] Join room";
+
+    println!("{}", row);
+    println!("{}", welcome_line);
+    println!("{}", author_line);
+    println!("{}\n", row);
+    println!("{}", create_server_option);
+    println!("{}", remove_server_option);
+    println!("{}\n", join_room_option);
+
+    println!("Please select an option: ");
+
+    let mut option = String::new();
+
+    io::stdin().read_line(&mut option)
+        .expect("Failed to read line");
+
+    let option_num: u32 = match option.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Unable to parse number");
+            return
+        },
+    };
+
+    match option_num {
+        1 => println!("Creating server..."),
+        2 => println!("Removing server..."),
+        3 => println!("Joining room..."),
+        _ => println!("Unknown option")
+    }
 }
