7dc0b7fdcf79d75836b4447fbe8e1a236cc79791
Dimeji Ogunyoye <dimejiogunyoye@googlemail.com>
2018-09-03 23:41:48 +0100

    Refactoring to later enable client to room pairing

--- src/client/client_manager_impl.rs
+++ src/client/client_manager_impl.rs
@@ -18,9 +18,11 @@ impl ClientManager for ClientManagerImpl {
     fn join_server(&mut self, username:String, sock_addr:SocketAddr) {
         println!("Joining server: {} as {}", sock_addr, username);
         let url = "ws://".to_owned() + &sock_addr.to_string();
+
         connect(url, |out| {
             let mut message = String::new();
             io::stdin().read_line(&mut message).expect("Failed to read message");
+            out.send(message.trim());
             out.send(message.trim());
 
             move |msg| {
--- src/server/server_manager_impl.rs
+++ src/server/server_manager_impl.rs
@@ -1,4 +1,4 @@
-use ws::{listen, CloseCode, Sender, Handler, Message};
+use ws::{listen, CloseCode, Sender, Handler, Handshake, Message};
 
 use ws::Result as WSResult;
 
@@ -10,10 +10,16 @@ use server::server_manager::ServerManager;
 use std::result::Result;
 
 struct Server {
-    out: Sender
+    out: Sender,
+    rooms: HashMap<String, Vec<Sender>>
 }
 
 impl Handler for Server {
+
+    fn on_open(&mut self, _: Handshake) -> WSResult<()> {
+        println!("token in open {:?}", self.out.token());
+        Ok(())
+    }
 
     fn on_message(&mut self, msg: Message) -> WSResult<()> {
         println!("Server got message '{}'. ", msg);
@@ -27,13 +33,8 @@ impl Handler for Server {
     }
 }
 
-struct ServerProperties {
-    thread_handle: JoinHandle<()>,
-    rooms: HashMap<String, Vec<Sender>>
-}
-
 pub struct ServerManagerImpl {
-    servers: HashMap<String, ServerProperties>
+    servers: HashMap<String, JoinHandle<()>>
 }
 
 pub trait ServerManagerFactory {
@@ -53,20 +54,22 @@ impl ServerManager for ServerManagerImpl {
 
     fn add_server(&mut self, port:u32) {
         println!("Adding server on port {}", port);
+
         let sock_addr = "127.0.0.1:".to_owned() + &port.to_string();
         let key = sock_addr.clone();
 
-        let join_handle: JoinHandle<_> = thread::spawn(move || listen(sock_addr, |out|
-            Server { out }
+        let mut rooms : HashMap<String, Vec<Sender>> = HashMap::new();
+        let default_list : Vec<Sender> = Vec::new();
+
+        rooms.insert("default".to_string(), default_list);
+
+        let join_handle: JoinHandle<_> =
+            thread::spawn(move || listen(sock_addr, |out|
+                Server { out, rooms: rooms.clone()}
         ).unwrap());
 
-        let property = ServerProperties {
-            thread_handle: join_handle,
-            rooms: HashMap::new()
-        };
-
         if !self.servers.contains_key(&key) {
-            self.servers.insert(key, property);
+            self.servers.insert(key, join_handle);
         }
 
     }
@@ -75,15 +78,15 @@ impl ServerManager for ServerManagerImpl {
         println!("Removing server on port {}", port);
 
         let sock_addr = "127.0.0.1:".to_owned() + &port.to_string();
-        let value: Option<ServerProperties> = self.servers.remove(&sock_addr);
+        let value: Option<JoinHandle<()>> = self.servers.remove(&sock_addr);
 
         if value.is_none() {
             let error_messsage = format!("Server on port {} does not exist", port);
             return Err(error_messsage);
         }
 
-        let properties = value.unwrap();
-        let join_result = properties.thread_handle.join();
+        let thread_handle = value.unwrap();
+        let join_result = thread_handle.join();
 
         if join_result.is_ok() {
             let ok_message = format!("Server on port {} removed", port);
